50a641d2be97c13ed3dbb5ef039fc0dafbb30ce7
fathi-asma <fathuasma456@gmail.com>
2025-01-16 11:27:19 +0530

    Add files via upload

---
+++ string manipulation part-1/readme.md
@@ -0,0 +1,59 @@
+# Write a program that performs the following string manipulations:
+
+### 1. Reverses a string.
+
+### 2. Slices a string between specified indices (start and end).
+
+### 3. Finds the index of a given character in the string.
+
+
+## Use the following methods:
+
+
+ -   `reverse(String value)` 
+ 
+  Reverses the input string.
+
+ - `slice(String value, int start, int end)` 
+
+ Slices the input string from the given start index (inclusive) to the end index (exclusive).
+
+- `indexOf(String value, char find)` 
+
+ Finds the index of the specified character in the input string.
+
+## The program should prompt the user for the following inputs:
+
+#### 1. A string to manipulate.
+
+#### 2. A start and end index for slicing.
+
+#### 3. A character to search for in the string.
+
+
+## Once the inputs are provided, the program should output:
+
+#### 1. The reversed string.
+
+#### 2. The sliced string.
+
+#### 3. The index of the specified character.
+
+
+## Sample Input:
+
+![](./images/1.jpg)
+
+![](./images/2.jpg)
+
+![](./images/3.jpg)
+
+
+
+## Sample Output:
+
+![](./images/4.jpg)
+
+![](./images/5.jpg)
+
+![](./images/6.jpg)
---
+++ string manipulation part-1/stringManipulationPart1.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+public class stringManipulationPart1{
+	
+	//Method to reverse a string
+	public static String reverse(String value) {
+		String reversed = "";
+		for (int i = value.length() - 1; i >= 0; i--) {
+			reversed += value.charAt(i);
+		}
+		return reversed;
+	}
+	
+	//Method to slice a string
+	public static String slice(String value, int start, int end) {
+		String sliced = "";
+		for (int i = start; i < end; i++) {
+			sliced += value.charAt(i);
+		}
+		return sliced;
+	}
+	
+	//Method to find index of the char
+	public static int indexOf(String value, char find) {
+		for (int i = 0; i < value.length(); i++) {
+			if (value.charAt(i) == find) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	//Main method
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		// prompt the user for input
+		System.out.print("Enter a string:");
+		String inputString = input.nextLine();
+		
+		System.out.print("Enter the start index for slicing:");
+		int start = input.nextInt();
+		
+		System.out.print("Enter the end index for slicing:");
+		int end = input.nextInt();
+		
+		System.out.print("Enter a character to find:");
+		char findChar = input.next().charAt(0);
+		
+		System.out.println();
+		
+		//use the methode and display results
+		String reversed = reverse(inputString);
+		System.out.println("Reversed String: " + reversed);
+		
+		String sliced = slice(inputString, start, end);
+		System.out.println("Sliced String: " + sliced);
+		
+		int index = indexOf(inputString, findChar);
+		System.out.println("Index of'"+ findChar + "':" +index);
+		
+		input.close ();
+	}
+}
+
+		
